9baf837d776ebe2689cf070c2370f23efd2624f5
AntonovAndrei <camuy4onkoa4a@yandex.ru>
2021-12-08 14:54:14 +0300

    Finish project

---
+++ diningPhilosophers/Fork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace diningPhilosophers
+{
+    public class Fork
+    {
+        public string Number { get; set; }
+        public bool IsFree { get; set; }
+        
+        //в чем разница между мьютексом и лок
+        public Mutex mutexObj = new Mutex();
+
+        public Fork(string number)
+        {
+            Number = number;
+            IsFree = true;
+        }
+        
+        //возможно более правильный
+        //public Semaphore semaphore = new Semaphore(0, 2);
+    }
+}
